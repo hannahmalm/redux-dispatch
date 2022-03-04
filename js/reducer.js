fbe79173b4a8235704ff818b50e7af184ebace29
Hannah Malm <hannahemalm616@gmail.com>
2022-03-04 10:06:07 -0600

    commit tree 361e233bab85c725f580ed35e41592310d54281e

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,38 @@
-function changeState(state, action){
+// function changeState(state, action){
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1}
+//     default:
+//       return state;
+//   }
+// }
+
+// let state = {count: 0}
+// let action = {type: 'INCREASE_COUNT'}
+
+// changeState(state, action)
+
+//start state at 0
+let state = { count: 0 };
+//always pass in the initial state and the action
+//create a switch case and increase the count by 1 if the action type is increase count
+function changeState(state, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
     default:
       return state;
   }
 }
+//in the document body render the state count
+function render() {
+  document.body.textContent = state.count;
+}
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+//after this dispatch the acton and then render
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
 
-changeState(state, action)
+render();
